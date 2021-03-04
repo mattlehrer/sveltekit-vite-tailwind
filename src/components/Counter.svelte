7508a3ccddac2f6558c0b936200b9a420afa852d
Matt Lehrer <lehrerm@gmail.com>
2021-03-03 22:24:43 -0800

    feat: setup tailwind

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -6,29 +6,9 @@
 	};
 </script>
 
-<button on:click={increment}>
+<button
+	on:click={increment}
+	class="py-4 px-8 bg-red-300 text-red-900 rounded-full outline-none tabular-nums focus:border-2 focus:border-red-600 active:bg-red-100"
+>
 	Clicks: {count}
 </button>
-
-<style>
-	button {
-		font-family: inherit;
-		font-size: inherit;
-		padding: 1em 2em;
-		color: #ff3e00;
-		background-color: rgba(255, 62, 0, 0.1);
-		border-radius: 2em;
-		border: 2px solid rgba(255, 62, 0, 0);
-		outline: none;
-		width: 200px;
-		font-variant-numeric: tabular-nums;
-	}
-
-	button:focus {
-		border: 2px solid #ff3e00;
-	}
-
-	button:active {
-		background-color: rgba(255, 62, 0, 0.2);
-	}
-</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	purge: ['src/app.html', 'src/**/*.svelte'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {
+			backgroundColor: ['active'],
+		},
+	},
+	plugins: [],
+};
