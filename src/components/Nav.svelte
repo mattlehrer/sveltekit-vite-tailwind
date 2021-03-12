57d2f31fadcb9914919d912d354f475739f83bc9
Matt Lehrer <lehrerm@gmail.com>
2021-03-12 11:19:17 -0800

    revert: remove console.log
    and replace with empty block with empty string per discord recommendation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,8 +1,10 @@
 <script>
-	console.log('in nav');
+	{
+		('');
+	}
 </script>
 
-<nav class="px-8 flex my-4 space-x-8 underline">
+<nav class="flex px-8 my-4 space-x-8 underline">
 	<a href="/">Home</a>
 	<a href="/about">About</a>
 </nav>
