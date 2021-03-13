4838dc7e4f874a825078d35975711778ea3ca76d
Matt Lehrer <lehrerm@gmail.com>
2021-03-13 15:00:28 -0800

    fix: temporary fix for vite sourcemap bug

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,9 +1,15 @@
+<script>
+	{
+		(''); // keep something to prevent Vite empty block sourcemap bug
+	}
+</script>
+
 <h1 class="mx-32 my-16">
 	This is the about page! It's setup with pseudoclasses and some dark mode
 	changes.
 </h1>
 
-<style>
+<style lang="postcss">
 	h1 {
 		@apply text-3xl text-indigo-600 hover:text-black dark:text-indigo-300 dark:hover:bg-indigo-800 px-2 dark:hover:text-indigo-100;
 		width: fit-content;
