7580471a9540e6030e53b3b439c181f1f5077fba
Matt Lehrer <lehrerm@gmail.com>
2021-03-14 15:19:03 -0700

    feat: add github link

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -4,11 +4,30 @@
 	}
 </script>
 
-<h1
-	class="mx-32 my-16 text-3xl text-indigo-600 hover:text-black dark:text-indigo-300 dark:hover:bg-indigo-800 px-2 dark:hover:text-indigo-100"
->
-	This is the about page!
-</h1>
+<div class="mx-32 my-16">
+	<h1
+		class="text-3xl text-indigo-600 hover:text-black dark:text-indigo-300 dark:hover:bg-indigo-800 px-2 dark:hover:text-indigo-100 -ml-2"
+	>
+		This is the about page.
+	</h1>
+	<p class="mt-8 underline">
+		<a href="https://github.com/mattlehrer/sveltekit-vite-tailwind">
+			The code for this SvelteKit starter template is available on GitHub.<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				class="h-5 w-5 inline-block ml-2 align-text-bottom"
+				fill="currentColor"
+			>
+				<path
+					d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+				/>
+				<path
+					d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+				/>
+			</svg>
+		</a>
+	</p>
+</div>
 
 <style lang="postcss">
 	h1 {
