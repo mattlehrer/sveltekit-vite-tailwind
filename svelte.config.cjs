440d02b8e12b601cd7ac285dbadc7a481ef40ca4
Matt Lehrer <lehrerm@gmail.com>
2021-03-24 15:45:09 -0700

    update to match current sveltekit scaffold

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,24 +1,19 @@
 const preprocess = require('svelte-preprocess');
 const pkg = require('./package.json');
-const { resolve } = require('path');
+const node = require('@sveltejs/adapter-node');
 
 module.exports = {
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: '@sveltejs/adapter-node',
+		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
 			ssr: {
 				noExternal: Object.keys(pkg.dependencies || {}),
-			},
-			resolve: {
-				alias: {
-					$components: resolve('src/components'),
-				},
 			},
 		},
 	},
