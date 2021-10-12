75fa4729cb6c7c5a57aeaaac6d37faef3763d8e2
Matt Lehrer <lehrerm@gmail.com>
2021-10-12 12:55:12 +0200

    fix: main is already in layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 	import Counter from '$lib/Counter.svelte';
 </script>
 
-<main class="p-4 mx-auto text-center">
+<div class="p-4 mx-auto text-center">
 	<h1
 		class="max-w-xs mx-auto my-16 text-6xl font-thin text-red-500 uppercase sm:max-w-none"
 	>
@@ -14,4 +14,4 @@
 		Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
 		apps.
 	</p>
-</main>
+</div>
