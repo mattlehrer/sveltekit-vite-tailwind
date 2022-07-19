d72cac9197ecab5e335e95ed4294c7bdd92c635c
Matt Lehrer <lehrerm@gmail.com>
2022-07-19 15:35:02 +0200

    catch up with sveltekit changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,38 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		// hydrate: false,
-		// router: false,
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
-	},
-
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		// hydrate: false,
-		// router: false,
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
+		adapter: adapter(),
 	},
 };
 
